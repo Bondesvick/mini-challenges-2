8143a757d7b40ceab741d69af80b78dafe91201d
Nwike Ugochukwu <bondesvick@gmail.com>
2020-05-03 08:35:47 +0100

    improved the remove-dulplicate solution

--- src/remove-dulplicates/index.js
+++ src/remove-dulplicates/index.js
@@ -1,27 +1,17 @@
 function removeDuplicates(obj) {
 
     let theKeys = Object.keys(obj);
-    let theValues = Object.values(obj);
     let noDuplicate = []
     let returned = {}
-
-    theValues.forEach((element, i) => {
-      element.forEach(part => {
-        if (!noDuplicate.includes(part)) {
-          noDuplicate.push(part)
-        }
-      })
-    })
     
-    Object.keys(obj).reverse().forEach(element => {
+    theKeys.reverse().forEach(element => {
       returned[element] = []
       obj[element].forEach((ler) => {
-        noDuplicate.forEach((char, i) => {
-          if (ler == char && !(returned[element].includes(ler))) {
-            returned[element].push(noDuplicate[i])
-            noDuplicate[i] = null
-          }
-        })
+
+        if (!noDuplicate.includes(ler)){
+          returned[element].push(ler);
+          noDuplicate.push(ler)
+        }
 
       });
 
